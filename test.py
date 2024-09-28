48e3352ce11884770f0bcf1f09f244d0f0c90f3b
Gendalf71 <gdedenko@gmail.com>
2024-09-28 10:33:23 +0300

    finished project

--- test.py
+++ test.py
@@ -1,5 +1,8 @@
 import mymod
-print("Testting functions. Part-1")
+print("Testing functions. Part-1")
 print(mymod.mysum(1,2))
 print(mymod.mysub(1,2))
-
+print("Testing functions. Part-2")
+print(mymod.mymul(1,2))
+print(mymod.mydiv(1,2))
+print("Finished testing")
